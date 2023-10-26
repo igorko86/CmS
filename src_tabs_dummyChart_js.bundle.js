74ed0256ef832b35232a7ce52d69c1a8c43122c3
imocherad <ihor.mocherad@newfireglobal.com>
2023-10-26 07:58:13 +0300

    Updates

---
+++ src_tabs_dummyChart_js.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcms"] = self["webpackChunkcms"] || []).push([["src_tabs_dummyChart_js"],{
+
+/***/ "./src/tabs/dummyChart.js":
+/*!********************************!*\
+  !*** ./src/tabs/dummyChart.js ***!
+  \********************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar DummyChart = function DummyChart() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \"Dummy Chart\"));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (DummyChart);\n\n//# sourceURL=webpack://cms/./src/tabs/dummyChart.js?");
+
+/***/ })
+
+}]);
